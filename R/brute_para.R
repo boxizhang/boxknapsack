80b3dde46f76472812ef723133f611a124ed1d15
boxi zhang <bossyzhang@gmail.com>
2017-10-15 03:05:26 +0200

    revise1

--- R/brute_para.R
+++ R/brute_para.R
@@ -15,7 +15,7 @@
 #     w=sample(1:4000, size = n, replace = TRUE),
 #     v=runif(n = n, 0, 10000)
 #   )
-# brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+# brute_force_knapsack_para(x = knapsack_objects[1:8,], W = 3500)
 
 
 
